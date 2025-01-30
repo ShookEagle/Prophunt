6ddde29c2406e61e8a546da26374759394621f24
ShookEagle <shookeagle@gmail.com>
2025-01-30 13:38:25 -0600

    remove excessive files

--- mod/Prophunt.Messaging/Announcer.cs
+++
@@ -1,9 +0,0 @@
-using Prophunt.Public.Behaviors;
-using Prophunt.Public.Mod.Messaging;
-
-namespace Prophunt.Messaging;
-
-public class Announcer(IMessenger messenger) : IPluginBehavior, IAnnouncer
-{
-    
-}
